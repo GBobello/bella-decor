8bb840cecb3ce7f63444b343a53a839f553eef2e
GBobello <bobello.gabriel@gmail.com>
2024-02-22 18:55:48 -0300

    Making some modifications because the development environment will be different.

--- src/pages/headboards.tsx
+++ src/pages/headboards.tsx
@@ -8,21 +8,18 @@ export function Headboards(){
 
         <span className="w-full p-4 mt-10 text-center text-2xl font-bold mb-10" >Cabeceiras</span>
 
-        <div className="flex flex-col justify-center mb-96">
-
-          <div className="flex flex-row p-5">
-            <div className="bg-red-500 w-64 h-24">
-              teste
+        <div className="flex justify-center items-center bg-gray-100 p-4">
+          <div className="w-full">
+            <div className="bg-red-400 md:aspect-square h-64 flex justify-center items-center">
+              <span className="text-white text-xl">Imagem do Produto</span>
             </div>
-            <div className="bg-lime-500 h-24 ml-5">
-              <h2>MODELO</h2>
-              <p className="text-sm " >The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested.</p>
+          </div>
+          <div className="w-full p-4 md:justify-start">
+            <div className="bg-green-300 h-64 flex justify-center items-center">
+              <span className="text-white text-xl">Descrição do produto</span>
             </div>
-          </div>    
-
-        </div>
-
-
+          </div>
+        </div>          
 
         <Footer />
       </div>
